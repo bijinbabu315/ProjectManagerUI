b0efc1be8f55c988a23ffd0d0f5c5085034c996e
BIJI N BABU <bijinbabu315@gmail.com>
2020-07-04 15:27:36 +0530

    Added spec for user comonents

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -1,14 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ListFilter } from 'src/app/list-filter.pipe';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/model/user';
+import { of } from 'rxjs';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let userService: UserService;
+  const formBuilder: FormBuilder = new FormBuilder();
 
+  const newUser: User = {
+    id: 1,
+    firstName: 'Jijil',
+    lastName: 'Kakkadathu',
+    employeeId: '87896',
+    projectId: '',
+    taskId: ''
+  };
+
+  const userList = [
+    {
+      id: 1,
+      firstName: 'Jijil',
+      lastName: 'Kakkadathu',
+      employeeId: '87896',
+      projectId: '',
+      taskId: ''
+    },
+    {
+      id: 2,
+      firstName: 'Achu',
+      lastName: 'Kurian',
+      employeeId: '75489',
+      projectId: '',
+      taskId: ''
+    }
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
+      imports: [ FormsModule, ReactiveFormsModule, HttpClientTestingModule],
+      declarations: [ UserComponent, ListFilter ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      providers: [UserService, { provide: FormBuilder, useValue: formBuilder }]
     })
     .compileComponents();
   }));
@@ -17,9 +56,66 @@ describe('UserComponent', () => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    userService = TestBed.get(UserService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return form as false', () => {
+    const controlsInForm = component.formControls;
+    controlsInForm.firstName.setValue('');
+    component.userForm.controls.firstName.setValue(null);
+    fixture.detectChanges();
+    component.addUser();
+    expect(component.userForm.valid).toBeFalse();
+  });
+
+  it('should call add user method', () => {
+    component.userForm = formBuilder.group(newUser);
+    component.addUser();
+    expect(component.submitted).not.toBeFalsy();
+  });
+
+  it('should reset form', () => {
+    component.resetForm();
+    expect(component.submitted).toBeFalsy();
+  });
+
+  it('should call edit user', () => {
+    component.editUser(newUser);
+    expect(component.submitButtonText).toEqual('Update');
+  });
+
+  it('should call addOrEditUser', () => {
+    spyOn(userService, 'addOrEditUser').and.returnValue(of(newUser));
+    spyOn(userService, 'getAllUsers').and.returnValue(of(userList));
+    component.addOrEditUser(newUser);
+    expect(userService.addOrEditUser).toHaveBeenCalled();
+  });
+
+  it('should call delete user', () => {
+    spyOn(userService, 'deleteUser').and.returnValue(of(newUser));
+    component.deleteUser('1');
+    expect(userService.deleteUser).toHaveBeenCalled();
+  });
+
+  it('should sort the list with first name', () => {
+    component.userList = userList;
+    component.sortUserList('firstName');
+    expect(component.userList[0].firstName).toEqual('Achu');
+  });
+
+  it('should sort the list with last name', () => {
+    component.userList = userList;
+    component.sortUserList('lastName');
+    expect(component.userList[0].firstName).toEqual('Jijil');
+  });
+
+  it('should sort the list with id', () => {
+    component.userList = userList;
+    component.sortUserList('id');
+    expect(component.userList[0].id).toEqual(1);
   });
 });
--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,16 +1,83 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { User } from '../model/user';
+import { environment } from 'src/environments/environment';
+import { UserServiceURLS } from '../constants/service.constant';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
+  const baseUrl = environment.apiUrl;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return the list of added users', () => {
+    const userList = [
+      {
+        id: 1,
+        firstName: 'Biji',
+        lastName: 'Babu',
+        employeeId: '604857'
+      },
+      {
+        id: 2,
+        firstName: 'Sobin',
+        lastName: 'Yohannan',
+        employeeId: '589632'
+      }
+    ];
+    let response;
+
+    spyOn(service, 'getAllUsers').and.returnValue(of(userList));
+
+    service.getAllUsers().subscribe(userData => {
+      response = userData;
+    });
+
+    expect(response).toEqual(userList);
+  });
+
+  it('should add or edit a user', () => {
+    const user: User = {
+      id: 1,
+      firstName: 'Sibi',
+      lastName: 'Babu',
+      employeeId: '658932',
+      projectId: '',
+      taskId: ''
+    };
+    service.addOrEditUser(user).subscribe(userInfo => {
+      expect(userInfo).toBe(user);
+    });
+    const req = httpMock.expectOne(baseUrl + UserServiceURLS.SAVE_OR_UPDATE);
+    expect(req.request.method).toBe('POST');
+    req.flush(user);
+  });
+
+  it('should delete a user', () => {
+    service.deleteUser('1').subscribe(resp => {
+      expect(resp).toBeTruthy();
+    });
+    const req = httpMock.expectOne(baseUrl + UserServiceURLS.DELETE_USER + '/1');
+    expect(req.request.method).toBe('DELETE');
+  });
+
 });
+
